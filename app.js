ded1407677ee0fee1844f2b6cb9ec897cb74bf96
Denise Judd <djudd0720@yahoo.com>
2020-03-01 10:19:11 -0500

    Using OAUTH and sessions

--- app.js
+++ app.js
@@ -9,7 +9,9 @@ const mongoose = require("mongoose");
 const session = require('express-session');
 const passport = require("passport");
 const passportLocalMongoose = require("passport-local-mongoose");
-
+//to use oauth must require below + configure
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const findOrCreate = require('mongoose-findorcreate')
 
 
 const app = express();
@@ -35,21 +37,59 @@ mongoose.set("useCreateIndex", true);
 
 const userSchema = new mongoose.Schema ({
   email: String,
-  password: String
+  password: String,
+  googleId: String,
+  secret: String
 });
 
 userSchema.plugin(passportLocalMongoose); //using to salt and hash our passwords and save our unser to db
+userSchema.plugin(findOrCreate); //add as plugin to use findOrCreate function
 
 const User = new mongoose.model("User", userSchema);
 
 passport.use(User.createStrategy());
 
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+  User.findById(id, function(err, user) {
+    done(err, user);
+  });
+});
+
+//configure oauth20
+passport.use(new GoogleStrategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/secrets",
+    //to fix deprecation errors from google+ being sunsetted
+    userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    console.log(profile);
+    User.findOrCreate({ googleId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
 
 app.get("/", function(req, res){
   res.render("home");
 });
+
+//User select register or login using Google
+app.get("/auth/google",
+  passport.authenticate('google', { scope: ["profile"] }
+));
+
+app.get("/auth/google/secrets",
+  passport.authenticate('google', { failureRedirect: '/login' }),
+  function(req, res) {
+    // Successful authentication, redirect secrets page.
+    res.redirect("/secrets");
+  });
 
 app.get("/login", function(req, res){
   res.render("login");
@@ -59,10 +99,23 @@ app.get("/register", function(req, res){
   res.render("register");
 });
 
-//this route checks to see if the user is authenticated and if so, sends to secrets page, otherwise redirects them to the login page to try again
+//gather all secrets from database and render
 app.get("/secrets", function(req, res){
+  User.find({"secret": {$ne:null}}, function(err, foundUsers) {
+    if (err) {
+      console.log(err);
+    } else {
+      if (foundUsers) {
+        res.render("secrets", {userWithSecrets: foundUsers}); //will render secrets.ejs page with all secrets not null - passing in variable we created for the secrets and then pass in foundUsers
+      }
+    }
+  });
+});
+
+//check to see if user is authenticated before submitting a secret
+app.get("/submit", function(req, res) {
   if (req.isAuthenticated()) {
-    res.render("secrets");
+    res.render("submit");
   } else{
     res.redirect("/login");
   }
@@ -113,6 +166,25 @@ app.post("/login", function(req, res){
     }
   });
 
+});
+
+app.post("/submit", function(req, res) {
+  const submittedSecret = req.body.secret;
+
+  console.log(req.user.id);
+
+  User.findById(req.user.id, function(err, foundUser) {
+    if (err){
+      console.log(err);
+    } else {
+      if (foundUser) {
+        foundUser.secret = submittedSecret;
+        foundUser.save(function() {
+          res.redirect("/secrets");
+        });
+      }
+    }
+  });
 });
 
 
